45e6c2d42b95e453b678c1955368994a0738e3cb
李鑫鹏 <283534181@qq.com>
2017-06-12 17:50:21 +0800

    首页默认跳转

---
+++ tianrui-wuliu-web/src/main/java/com/tianrui/web/action/IndexAction.java
@@ -0,0 +1,22 @@
+package com.tianrui.web.action;
+
+import java.io.IOException;
+
+import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+@RequestMapping("/")
+public class IndexAction {
+	
+	public static Logger loger=org.slf4j.LoggerFactory.getLogger(IndexAction.class);
+	
+	//成为货主申请
+	@RequestMapping("")
+	public ModelAndView index() throws IOException{
+		ModelAndView model =new ModelAndView("/index");
+		return model;
+	}
+}
