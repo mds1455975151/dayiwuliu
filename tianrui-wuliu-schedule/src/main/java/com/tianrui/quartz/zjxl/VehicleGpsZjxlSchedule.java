57c66fbdb4595f18491fc24108cb86a54ca01a14
李鑫鹏 <283534181@qq.com>
2017-08-18 14:13:11 +0800

    中交行路对接不完成版本

---
+++ tianrui-wuliu-schedule/src/main/java/com/tianrui/quartz/zjxl/VehicleGpsZjxlSchedule.java
@@ -0,0 +1,15 @@
+package com.tianrui.quartz.zjxl;
+
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component  
+public class VehicleGpsZjxlSchedule {
+
+	
+	
+	//@Scheduled(cron="0 0 1 * * ?")
+	public  void auditReport()throws Exception{
+		
+	}
+}
---
+++ tianrui-wuliu-service/src/main/java/com/tianrui/service/bean/VehicleGpsZjxl.java
@@ -0,0 +1,88 @@
+package com.tianrui.service.bean;
+
+import java.io.Serializable;
+
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import com.tianrui.common.utils.DateUtil;
+
+@Document(collection = "vehicle_gps_zjxl")
+public class VehicleGpsZjxl implements Serializable{
+
+	private static final long serialVersionUID = 5579767148660524131L;
+
+	@Id
+	private String id;
+	
+	//图片 上传时间
+	private long timeStamp;
+	//时间戳
+	private String createTime =DateUtil.getDateString();
+	//utc时间
+	private String utcTime ;
+	
+	private Long lat;
+	private Long lon;
+	private Long utc;
+	private String vehicleNo;
+	private String addr;
+	private String spd;
+	
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	public String getCreateTime() {
+		return createTime;
+	}
+	public void setCreateTime(String createTime) {
+		this.createTime = createTime;
+	}
+	public Long getLat() {
+		return lat;
+	}
+	public void setLat(Long lat) {
+		this.lat = lat;
+	}
+	public Long getLon() {
+		return lon;
+	}
+	public void setLon(Long lon) {
+		this.lon = lon;
+	}
+	public Long getUtc() {
+		return utc;
+	}
+	public void setUtc(Long utc) {
+		this.utc = utc;
+	}
+	public String getVehicleNo() {
+		return vehicleNo;
+	}
+	public void setVehicleNo(String vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+	public String getAddr() {
+		return addr;
+	}
+	public void setAddr(String addr) {
+		this.addr = addr;
+	}
+	public String getSpd() {
+		return spd;
+	}
+	public void setSpd(String spd) {
+		this.spd = spd;
+	}
+	
+	
+}
