e6d70c1eaa70ad71049f19b6479a83dcc19c8423
yang526629@163.con <yang526629@163.con>
2017-10-17 17:08:38 +0800

    运单、计划、支付统计实体类

---
+++ tianrui-wuliu-service/src/main/java/com/tianrui/service/bean/PayCount.java
@@ -0,0 +1,96 @@
+package com.tianrui.service.bean;
+
+public class PayCount {
+	
+	private String trueWeightCount;
+
+    private String priceCount;
+
+    private String totalPriceCount;
+
+    private String oilCardCount;
+
+    private String weightMiscCount;
+
+    private String deductMoneyCount;
+
+    private String deductOtherCount;
+
+    private String amountPayableCount;
+
+    private String paidAmountCount;
+
+	public String getTrueWeightCount() {
+		return trueWeightCount;
+	}
+
+	public void setTrueWeightCount(String trueWeightCount) {
+		this.trueWeightCount = trueWeightCount;
+	}
+
+	public String getPriceCount() {
+		return priceCount;
+	}
+
+	public void setPriceCount(String priceCount) {
+		this.priceCount = priceCount;
+	}
+
+	public String getTotalPriceCount() {
+		return totalPriceCount;
+	}
+
+	public void setTotalPriceCount(String totalPriceCount) {
+		this.totalPriceCount = totalPriceCount;
+	}
+
+	public String getOilCardCount() {
+		return oilCardCount;
+	}
+
+	public void setOilCardCount(String oilCardCount) {
+		this.oilCardCount = oilCardCount;
+	}
+
+	public String getWeightMiscCount() {
+		return weightMiscCount;
+	}
+
+	public void setWeightMiscCount(String weightMiscCount) {
+		this.weightMiscCount = weightMiscCount;
+	}
+
+	public String getDeductMoneyCount() {
+		return deductMoneyCount;
+	}
+
+	public void setDeductMoneyCount(String deductMoneyCount) {
+		this.deductMoneyCount = deductMoneyCount;
+	}
+
+	public String getDeductOtherCount() {
+		return deductOtherCount;
+	}
+
+	public void setDeductOtherCount(String deductOtherCount) {
+		this.deductOtherCount = deductOtherCount;
+	}
+
+	public String getAmountPayableCount() {
+		return amountPayableCount;
+	}
+
+	public void setAmountPayableCount(String amountPayableCount) {
+		this.amountPayableCount = amountPayableCount;
+	}
+
+	public String getPaidAmountCount() {
+		return paidAmountCount;
+	}
+
+	public void setPaidAmountCount(String paidAmountCount) {
+		this.paidAmountCount = paidAmountCount;
+	}
+    
+    
+}
---
+++ tianrui-wuliu-service/src/main/java/com/tianrui/service/bean/PlanCount.java
@@ -0,0 +1,47 @@
+package com.tianrui.service.bean;
+
+public class PlanCount {
+		private String planWeightCount;
+	    private String complitWeightCount;
+	    private String tempoCount;
+	    private String distantCount;
+	    private String priceCount;
+	    private String taxCount;
+		public String getPlanWeightCount() {
+			return planWeightCount;
+		}
+		public void setPlanWeightCount(String planWeightCount) {
+			this.planWeightCount = planWeightCount;
+		}
+		public String getComplitWeightCount() {
+			return complitWeightCount;
+		}
+		public void setComplitWeightCount(String complitWeightCount) {
+			this.complitWeightCount = complitWeightCount;
+		}
+		public String getTempoCount() {
+			return tempoCount;
+		}
+		public void setTempoCount(String tempoCount) {
+			this.tempoCount = tempoCount;
+		}
+		public String getDistantCount() {
+			return distantCount;
+		}
+		public void setDistantCount(String distantCount) {
+			this.distantCount = distantCount;
+		}
+		public String getPriceCount() {
+			return priceCount;
+		}
+		public void setPriceCount(String priceCount) {
+			this.priceCount = priceCount;
+		}
+		public String getTaxCount() {
+			return taxCount;
+		}
+		public void setTaxCount(String taxCount) {
+			this.taxCount = taxCount;
+		}
+		
+}
