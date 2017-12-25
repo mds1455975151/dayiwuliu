05f699606308b1999f3598201084add4f8a38c2d
lsj <393630440@qq.com>
2017-12-25 10:59:20 +0800

    在途轨迹查询

--- tianrui-wuliu-api/src/main/java/com/tianrui/api/req/money/TrackSelectReq.java
+++ tianrui-wuliu-api/src/main/java/com/tianrui/api/req/money/TrackSelectReq.java
@@ -5,6 +5,22 @@ public class TrackSelectReq {
 	private Integer pageNo;
 	
 	private Integer pageSize;
+	
+	private String customerName;//司机姓名
+
+    private String cellphone;//司机账号
+
+    private String contactNumber;//联系电话
+
+    private String vehicleNo;//车牌号
+    
+    private Byte problemType;//问题类型1-轨迹异常
+    
+    private Byte solvingState;//处理状态0-待处理，1-处理中，2-已处理
+    
+    private Long createTimeBegin;//预警时间开始
+    
+    private Long createTimeEnd;//预警时间结束
 
 	public Integer getPageNo() {
 		return pageNo;
@@ -20,6 +36,70 @@ public class TrackSelectReq {
 
 	public void setPageSize(Integer pageSize) {
 		this.pageSize = pageSize;
+	}
+
+	public String getCustomerName() {
+		return customerName;
+	}
+
+	public void setCustomerName(String customerName) {
+		this.customerName = customerName;
+	}
+
+	public String getCellphone() {
+		return cellphone;
+	}
+
+	public void setCellphone(String cellphone) {
+		this.cellphone = cellphone;
+	}
+
+	public String getContactNumber() {
+		return contactNumber;
+	}
+
+	public void setContactNumber(String contactNumber) {
+		this.contactNumber = contactNumber;
+	}
+
+	public String getVehicleNo() {
+		return vehicleNo;
+	}
+
+	public void setVehicleNo(String vehicleNo) {
+		this.vehicleNo = vehicleNo;
+	}
+
+	public Byte getProblemType() {
+		return problemType;
+	}
+
+	public void setProblemType(Byte problemType) {
+		this.problemType = problemType;
+	}
+
+	public Byte getSolvingState() {
+		return solvingState;
+	}
+
+	public void setSolvingState(Byte solvingState) {
+		this.solvingState = solvingState;
+	}
+
+	public Long getCreateTimeBegin() {
+		return createTimeBegin;
+	}
+
+	public void setCreateTimeBegin(Long createTimeBegin) {
+		this.createTimeBegin = createTimeBegin;
+	}
+
+	public Long getCreateTimeEnd() {
+		return createTimeEnd;
+	}
+
+	public void setCreateTimeEnd(Long createTimeEnd) {
+		this.createTimeEnd = createTimeEnd;
 	}
 
 }
