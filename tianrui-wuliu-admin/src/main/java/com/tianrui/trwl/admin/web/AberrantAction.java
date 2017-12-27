bb0b4202477dff3732c749231652a76e298bbdee
wfl <776532455@qq.com>
2017-12-27 08:46:16 +0800

    异常处理

--- tianrui-wuliu-admin/src/main/java/com/tianrui/trwl/admin/web/AberrantAction.java
+++ tianrui-wuliu-admin/src/main/java/com/tianrui/trwl/admin/web/AberrantAction.java
@@ -1,15 +1,21 @@
 package com.tianrui.trwl.admin.web;
+import javax.servlet.http.HttpServletRequest;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.servlet.ModelAndView;
 
+import com.tianrui.api.message.intf.IMessageGroupService;
+import com.tianrui.api.req.groupMsg.PushGroupMessageReq;
 import com.tianrui.api.req.money.TrackSelectReq;
 import com.tianrui.api.resp.money.CustomRcordResp;
 import com.tianrui.api.tracking.intf.ITrackingService;
 import com.tianrui.common.vo.PaginationVO;
 import com.tianrui.common.vo.Result;
+import com.tianrui.service.admin.bean.Users;
+import com.tianrui.trwl.admin.util.SessionManager;
 
 
 @Controller
@@ -18,6 +24,8 @@ public class AberrantAction {
 	
 	@Autowired
 	ITrackingService trackingService;
+	@Autowired
+	IMessageGroupService messageGroupService;
 	
 	@RequestMapping("/aberdeal")//浏览器访问路径  /admin/aberrant  +  /aberdeal
 	public ModelAndView aberdeal(){
@@ -44,6 +52,18 @@ public class AberrantAction {
 		Result rs = Result.getSuccessResult();
 		PaginationVO<CustomRcordResp> page = trackingService.select(req);
 		rs.setData(page);
+		return rs;
+	}
+	
+	/** 发送消息推送
+	 * @throws Exception */
+	@RequestMapping("pushGroupMsg")
+	@ResponseBody
+	public Result pushGroupMsg(PushGroupMessageReq req,HttpServletRequest request) throws Exception{
+		Users user = SessionManager.getSessionMember(request);
+		Result rs = Result.getSuccessResult();
+		req.setSysUser(user.getAccount());
+		rs = messageGroupService.pushGroupMsg(req);
 		return rs;
 	}
 
