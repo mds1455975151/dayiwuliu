1e915d3df368284c4c0407e54fe695815371741b
cy <1379381703@qq.com>
2017-12-27 15:40:59 +0800

    banner

---
+++ tianrui-wuliu-admin/src/main/webapp/resources/js/appbanner.js
@@ -0,0 +1,39 @@
+$.ajax({
+    url:"/admin/banner/queryBanner",
+    dataType:'json',
+    type:'post',
+    success:function(result){
+        if(result.code == '000000'){
+        	var list = result.data||[];
+        	 for(var i=0;i<list.length;i++){
+        		 var str="",str2="";
+        		 if(list[i].pushStatus==0){
+        			 str='<p>发布状态：<span class="status red">待发布</span></p>'
+        		 }else{
+        			 str='<p>发布状态：<span class="status blue">已发布</span></p>'
+        		 }
+        		 if((list[i].picStatus)==1){
+                	 str2='<div class="count">启用</div><div class="count2">删除</div>'
+				}else{
+					str2='<div class="count1">禁用</div>'
+				}
+        	var html='<div class="border">'+
+             '<div class="info" data-id='+(list[i].id)+'>'+
+                '<img src='+(list[i].httpUrl)+'>'+
+                '<p>'+(list[i].picName)+'</p>'+
+                str2+
+                '<p>链接：'+(list[i].httpUrl)+'</p>'+
+                	str+
+                ' </div>'+
+                '</div>'
+                
+               
+                $('.infoBox').append(html);
+        	 } 
+            console.log(result) 
+        }
+    }
+})
+    $(".add").off('click').on('click',function(){
+    	window.location.href="/admin/banner/bannerAddPage";
+    })
---
+++ tianrui-wuliu-admin/src/main/webapp/resources/js/upload.js
@@ -0,0 +1,48 @@
+var iBytesUploaded = 0;
+var iBytesTotal = 0;
+var iPreviousBytesLoaded = 0;
+var iMaxFilesize = 1048576; // 1MB
+var oTimer = 0;
+var sResultFileSize = '';
+
+function uploadBase64(base64Str){
+	$.ajax({
+		url:"/upload/baes64Add",
+		type:"POST",
+		data:{imgStr:base64Str},
+		success:function(ret){
+			if(ret.code==000000){
+				alert(ret.data);
+			}else{
+				alert("上传失败");
+			}
+			
+		}
+	});
+}
+
+function fileSelected(a, b) {
+    var oFile = document.getElementById(b).files[0];
+    // filter for image files
+    var rFilter = /^(image\/bmp|image\/gif|image\/jpeg|image\/png|image\/tiff)$/i;
+    if (!rFilter.test(oFile.type)) {
+        document.getElementById('error').style.display = 'block';
+        return;
+    }
+    // get preview element
+    var oImage = document.getElementById(a);
+    console.log(a);
+    // prepare HTML5 FileReader
+    var oReader = new FileReader();
+    oReader.onload = function(e) {
+        oImage.src = e.target.result; 
+        uploadBase64(oImage.src);
+    };
+
+    // read selected file as DataURL
+    oReader.readAsDataURL(oFile);
+}
+    $(".queding").off('click').on('click',function(){
+    	alert(1);
+    })
+   
