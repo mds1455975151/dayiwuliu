c7f62ef06c9a667f0f2630a07c5c18320b58fdcd
lsj <393630440@qq.com>
2018-01-05 09:31:23 +0800

    生成xml文件

---
+++ tianrui-wuliu-service/src/main/java/com/tianrui/service/bean/SettingRoute.java
@@ -0,0 +1,83 @@
+package com.tianrui.service.bean;
+
+public class SettingRoute {
+    private Long id;
+
+    private String cargoId;
+
+    private String routename;
+
+    private String routeId;
+
+    private String desc1;
+
+    private String desc2;
+
+    private String desc3;
+
+    private String desc4;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getCargoId() {
+        return cargoId;
+    }
+
+    public void setCargoId(String cargoId) {
+        this.cargoId = cargoId == null ? null : cargoId.trim();
+    }
+
+    public String getRoutename() {
+        return routename;
+    }
+
+    public void setRoutename(String routename) {
+        this.routename = routename == null ? null : routename.trim();
+    }
+
+    public String getRouteId() {
+        return routeId;
+    }
+
+    public void setRouteId(String routeId) {
+        this.routeId = routeId == null ? null : routeId.trim();
+    }
+
+    public String getDesc1() {
+        return desc1;
+    }
+
+    public void setDesc1(String desc1) {
+        this.desc1 = desc1 == null ? null : desc1.trim();
+    }
+
+    public String getDesc2() {
+        return desc2;
+    }
+
+    public void setDesc2(String desc2) {
+        this.desc2 = desc2 == null ? null : desc2.trim();
+    }
+
+    public String getDesc3() {
+        return desc3;
+    }
+
+    public void setDesc3(String desc3) {
+        this.desc3 = desc3 == null ? null : desc3.trim();
+    }
+
+    public String getDesc4() {
+        return desc4;
+    }
+
+    public void setDesc4(String desc4) {
+        this.desc4 = desc4 == null ? null : desc4.trim();
+    }
+}
